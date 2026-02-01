82a9b6c9c5943e0abb9a009c009aa2b6a74afaa6
Fenkreg <fenkreg@yandex.ru>
2026-02-01 13:35:30 +0300

    Shelfs Colors Ramdomizer created

---
+++ AGENTS.md
@@ -0,0 +1,8 @@
+<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
+- Project name: Fran_Library
+- Unity version: Unity 6000.2.6f2
+- Active game object:
+  - Name: BookGenerator
+  - Tag: Untagged
+  - Layer: Default
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/_Scripts/BookGen.cs
+++ Assets/_Scripts/BookGen.cs
@@ -1,16 +1,63 @@
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BookGen : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Color[] colors = new Color[]
+    {
+        Color.aquamarine,
+        Color.cornflowerBlue,
+        Color.darkOrange,
+        Color.indianRed,
+        Color.mediumOrchid,
+        Color.navyBlue,
+        Color.plum,
+        Color.skyBlue,
+        Color.yellowNice
+    };
+    public GameObject[] markers;
+    private int[] colorChecker = new int[9] { 9,9,9,9,9,9,9,9,9 };
+    public int pickedColor;
+    public int currentMarker = 0;
+
     void Start()
     {
-        
+        MarkerColorPick();
     }
 
-    // Update is called once per frame
-    void Update()
+    void MarkerColorPick()
     {
-        
+
+        int pickedColor = Random.Range(0, colors.Length);
+        MarkerColorCheck();
+
+        void MarkerColorCheck()
+        {
+            if (!colorChecker.Contains(pickedColor))
+            {
+                //Debug.Log("!!!CHECKER pickedColor: " + pickedColor + " | colorChecker: " + colorChecker.ToCommaSeparatedString() + " | currentMarker: " + currentMarker);
+                MarkerColorSet();
+            }
+            if (colorChecker.Contains(pickedColor) && currentMarker < markers.Length)
+            {
+                //Debug.Log("_CHECKER pickedColor: " + pickedColor + " | colorChecker: " + colorChecker.ToCommaSeparatedString() + " | currentMarker: " + currentMarker);
+                MarkerColorPick();
+            }
+
+        }
+        void MarkerColorSet()
+        {
+            if (currentMarker <= markers.Length)
+            {
+                markers[currentMarker].GetComponent<Renderer>().material.color = colors[pickedColor];
+                colorChecker[currentMarker] = pickedColor;
+                ++currentMarker;
+                //Debug.Log("SET pickedColor: " + pickedColor + " | colorChecker: " + colorChecker.ToCommaSeparatedString() + " | currentMarker: " + currentMarker + " | colorChecker.Contains(pickedColor): " + colorChecker.Contains(pickedColor) + " | currentMarker < markers.Length: " + (currentMarker < markers.Length));
+                MarkerColorPick();
+            }
+
+        }
     }
+
 }

4303a68c4e502f3a7e76d90e80ecdd9f2a6f8231
Fenkreg <74467190+Fenkreg@users.noreply.github.com>
2026-03-29 19:24:12 +0300

    Completed all tasks related to book shelves

--- Assets/_Scripts/BookDropper.cs
+++ Assets/_Scripts/BookDropper.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BookDropper : MonoBehaviour
 {
@@ -23,9 +24,16 @@ public void GetDefaultComponents()
     {
         booksToPick = Random.Range(7,13);
         for (int i = 0; i < booksToPick; i++)
+        //Vector3 bookMove = 0f, 0f, 1.0f;
         {
         List<GameObject> randomList = booksList.OrderBy(x => Random.value).ToList();
-        randomList[i].SetActive(false); // this will be replaced by bookMovement function
+        
+        //Get random books, move them forward, rotate by RNG and enable physics
+        randomList[i].transform.Translate(Vector3.forward * -1.5f, Space.Self);
+        float bookRotation = Random.Range(-30f,30f); 
+        randomList[i].transform.Rotate(0f,0f, bookRotation,Space.Self);
+        randomList[i].GetComponent<Rigidbody>().isKinematic = false;
+
         //Debug.Log($"i= {i} | booksToPick = {booksToPick}");
         booksList.RemoveAt(i);
         }
